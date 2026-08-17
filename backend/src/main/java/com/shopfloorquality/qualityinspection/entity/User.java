19b500d9286c2074373ae19ec0a641fddbb0dd0e
akshubham63 <116741541+akshubham63@users.noreply.github.com>
2026-08-17 15:56:55 +0530

    Fixed column name for user role

--- backend/src/main/java/com/shopfloorquality/qualityinspection/entity/User.java
+++ backend/src/main/java/com/shopfloorquality/qualityinspection/entity/User.java
@@ -34,7 +34,7 @@ public class User implements UserDetails {
     @Column(nullable = false)
     private String fullName;
     
-    @Column(nullable = false)
+    @Column(name= "role", nullable = false)
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
     
